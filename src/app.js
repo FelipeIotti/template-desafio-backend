f68fb8ca7d157543dd45e54fab180082cdf987b7
Felipe Iotti <felipeizago@hotmail.com>
2020-10-30 21:56:11 -0300

    Project CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+ const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +10,78 @@ app.use(cors());
 
 const repositories = [];
 
+function validateProjectId(request,response,next){
+  const {id} = request.params;
+
+  if(!isUuid(id)){
+      return response.status(400).json({error: 'Invalid project ID.'});
+  }
+  return next();
+}
+
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} =request.body;
+
+  const repositorie ={id: uuid(), title,url,techs, likes:0,};
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id",validateProjectId, (request, response) => {
+  const {id} = request.params;
+  const {title, url, techs} =request.body;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if (repositorieIndex < 0){
+      return response.status(400).json({ error: 'Repositorie not found.'});
+  }
+
+  const repositorie ={
+      id,
+      title,
+      url,
+      techs,
+      likes:repositories[repositorieIndex].likes,
+  }
+  repositories[repositorieIndex]= repositorie;
+
+  return response.json(repositorie);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id",validateProjectId, (request, response) => {
+  const {id} = request.params;
+
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if (repositorieIndex < 0){
+      return response.status(400).json({ error: 'repositorie not found.'});
+  }
+
+  repositories.splice(repositorieIndex,1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like",validateProjectId, (request, response) => {
+  const {id} = request.params;
+  
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id == id);
+
+  if (repositorieIndex < 0){
+      return response.status(400).json({ error: 'Project not found.'});
+  }
+  repositories[repositorieIndex].likes ++;
+
+  return response.json(repositories[repositorieIndex]);
 });
 
 module.exports = app;
